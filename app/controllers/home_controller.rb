099f73b2f4daa7fa2369efdae09f2cd21ccb445a
Asrani-Aman <amanasrani1712@gmail.com>
2024-05-21 16:42:08 +0530

    creating a controller and the corresponding view file

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+    def index
+        @title_text = 'Radhe Radhe'
+        @subtitle_text = 'Hare Krishna Hare Krishna Krishna Krishna Hare Hare Hare Rama Hare Rama Rama Rama Hare Hare'
+    end
+end
