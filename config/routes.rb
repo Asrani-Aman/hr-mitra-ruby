8a18933902622eaf2f0a25c059b13e7572c4fbf9
Asrani-Aman <amanasrani1712@gmail.com>
2024-05-21 18:08:29 +0530

    creating about us contactus pages through the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
- # get 'home/index'
- get '/home' => 'home#index'
+  root 'home#index'
+  
+  # get 'pages/about-us'
+  # get 'pages/contact-us'
+
+  get 'about' => 'pages#about_us'
+  get 'contact' => 'pages#contact_us'
 end
