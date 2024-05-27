3a8d11c76a30815e1a764faa0cf5a6b2ad84cb92
Asrani-Aman <amanasrani1712@gmail.com>
2024-05-27 17:28:15 +0530

    create and save model in database

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,8 +1,31 @@
 class EmployeesController < ApplicationController
+
   def index
     @employees = Employee.all
   end
+
   def new
     @employee = Employee.new
   end
+
+  def create
+    @employee = Employee.new(allowed_employee_params)
+    if @employee.save
+      
+      redirect_to employees_path, notice: "Employee was successfully created."
+      flash[:notice] = "Employee was successfully created yeahhhhhhhh!!!!!!!!!!!!!!!."
+    else
+      flash[:notice] = "Employee was not successfully created."
+      flash.now[:error] = "Rats! Fix your mistakes, please."
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
+
+private
+# This method defines strong parameters, which allow only specific attributes to be passed in the params hash.
+# It ensures that only the permitted attributes can be used for mass assignment.
+def allowed_employee_params
+  params.require(:employee).permit(:first_name,:middle_name, :last_name, :personal_email, :city, :country, :state, :pincode, :address_line_1, :address_line_2)
+end
+
