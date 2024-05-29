7af420e571f16b323996182f04e5fdf9928d37dc
Asrani-Aman <amanasrani1712@gmail.com>
2024-05-29 10:54:37 +0530

    Dropdown for doc_type by using array

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,5 @@
 class Document < ApplicationRecord
+  DOC_TYPES = ['Marksheet', 'Aadhar Card', 'PAN Card', 'Driving License', 'Passport', 'Voter ID', 'Other'].freeze
   belongs_to :employee
   validates :name, :doc_type, presence: true
 end
