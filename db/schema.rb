3530e210a7081a877a0341536ef6f8f1b134acaa
Asrani-Aman <amanasrani1712@gmail.com>
2024-05-31 13:02:27 +0530

    remove middle name columne from the employees table through migration(remove_column method)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_31_051801) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_31_070859) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,7 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_31_051801) do
 
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
-    t.string "middle_name"
     t.string "last_name"
     t.string "personal_email"
     t.string "city"
