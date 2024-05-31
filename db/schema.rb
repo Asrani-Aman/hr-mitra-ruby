744e80163e028096d886c6bc10ae11383f57c566
Asrani-Aman <amanasrani1712@gmail.com>
2024-06-01 00:43:03 +0530

    rename existing column in the employees table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_31_070859) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_31_190722) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,7 +63,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_31_070859) do
     t.date "date_of_birth"
     t.string "job_title"
     t.date "date_of_joining"
-    t.text "bio"
+    t.text "about"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
